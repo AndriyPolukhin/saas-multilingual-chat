44e6c62dd10f19cb3cbe8ad5ccdee46ee459cd5a
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-08 23:59:11 +0200

    Added Logo component with ShandCn aspect ration element

---
+++ components/Logo.tsx
@@ -0,0 +1,26 @@
+import logoImage from '@logos/logo2.svg'
+import Link from 'next/link'
+import Image from 'next/image'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
+
+const Logo = () => {
+	return (
+		<Link href='/' prefetch={false} className='overflow-hidden'>
+			<div className='flex items-center w-72 h-14'>
+				<AspectRatio
+					ratio={16 / 9}
+					className='flex items-center justify-center'
+				>
+					<Image
+						priority
+						src={logoImage}
+						alt='logo'
+						className='dark:filter dark:invert'
+					/>
+				</AspectRatio>
+			</div>
+		</Link>
+	)
+}
+
+export default Logo
