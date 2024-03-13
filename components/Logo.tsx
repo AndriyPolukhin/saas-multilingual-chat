84ff7b84bfc7e674ff7e35f73d5aa846d5bb3b1a
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-13 22:53:36 +0200

    Added A userAvater component

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,4 +1,4 @@
-import logoImage from '@logos/logo.svg'
+import logoImage from '@logos/logo3.svg'
 import Link from 'next/link'
 import Image from 'next/image'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
