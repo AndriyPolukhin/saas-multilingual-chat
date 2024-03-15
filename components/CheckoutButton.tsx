9fb2e5b977fbf17bc66f561ae5f6b95e9e5c83f8
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-15 23:55:20 +0200

    Added Checkout Button and Register Page

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+'use client'
+
+import { useSession } from 'next-auth/react'
 
 const CheckoutButton = () => {
-	return <div className='bg-red-200 text-red-400'>CheckoutButton</div>
+	const { data: session } = useSession()
+	const createCheckoutSession = () => {
+		if (!session) return
+		// Push a document into firestore db
+
+		// ... stripe extension on firebase will create a checkout session
+
+		// redirect the user to the checkout page
+	}
+	return (
+		<div className='flex flex-col space-y-2'>
+			{/* IF Subscribed:  show me the user is subscribed */}
+			<button
+				onClick={() => createCheckoutSession()}
+				className='mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-60'
+			>
+				Sign Up
+			</button>
+		</div>
+	)
 }
 
 export default CheckoutButton
