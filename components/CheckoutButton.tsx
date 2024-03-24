d4f3a82cbcff0f26d1cebb2b5c20f46cbcb0ebb5
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-24 20:44:07 +0200

    Added subscription management via firebase/firestore and zustand

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,16 +1,42 @@
 'use client'
 
+import { db } from '@/firebase'
+import { addDoc, collection, onSnapshot } from 'firebase/firestore'
 import { useSession } from 'next-auth/react'
+import { useState } from 'react'
 
 const CheckoutButton = () => {
 	const { data: session } = useSession()
-	const createCheckoutSession = () => {
-		if (!session) return
+	const [loading, setLoading] = useState(false)
+	const createCheckoutSession = async () => {
+		if (!session?.user.id) return
 		// Push a document into firestore db
+		setLoading(true)
 
-		// ... stripe extension on firebase will create a checkout session
+		const docRef = await addDoc(
+			collection(db, 'customers', session.user.id, 'checkout_sessions'),
+			{
+				price: 'price_1OxbpJI2Mh4niaanTPDZQxjW',
+				success_url: window.location.origin,
+				cancel_url: window.location.origin,
+			}
+		)
 
-		// redirect the user to the checkout page
+		// stripe extension on firebase will create a checkout session
+		return onSnapshot(docRef, (snap) => {
+			const data = snap.data()
+			const url = data?.url
+			const error = data?.error
+			if (error) {
+				alert(`An error occured: ${error.message}`)
+				setLoading(false)
+			}
+			if (url) {
+				// redirect the user to the checkout page
+				window.location.assign(url)
+				setLoading(false)
+			}
+		})
 	}
 	return (
 		<div className='flex flex-col space-y-2'>
@@ -19,7 +45,7 @@ const CheckoutButton = () => {
 				onClick={() => createCheckoutSession()}
 				className='mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-60'
 			>
-				Sign Up
+				{loading ? 'loading...' : 'Sign Up'}
 			</button>
 		</div>
 	)
