3c4642ac7814f89e58cf4669a5831917f913c4bf
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-24 20:43:32 +0200

    Added Subscription store, converters and provider

---
+++ store/store.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand'
+import { Subscription } from '@/types/Subscription'
+
+export type LanguagesSupported =
+	| 'en'
+	| 'es'
+	| 'de'
+	| 'fr'
+	| 'ja'
+	| 'uk'
+	| 'pl'
+	| 'no'
+
+export const LanguagesSupportedMap: Record<LanguagesSupported, string> = {
+	en: 'English',
+	es: 'Spanish',
+	de: 'German',
+	fr: 'French',
+	ja: 'Japanese',
+	uk: 'Ukrainian',
+	pl: 'Polish',
+	no: 'Norwegian',
+}
+
+interface SubscriptionState {
+	subscription: Subscription | null | undefined
+	setSubscription: (subscription: Subscription | null) => void
+}
+
+export const useSubscriptionStore = create<SubscriptionState>((set) => ({
+	subscription: null,
+	setSubscription: (subscription: Subscription | null) => set({ subscription }),
+}))
