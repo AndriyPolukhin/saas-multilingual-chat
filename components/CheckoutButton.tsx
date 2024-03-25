7c6f59602fbd20cdad89e28c62f6ca5d3ce72ef8
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-26 00:01:25 +0200

    Added a portal Link to manage stripe subscription

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -5,10 +5,19 @@ import { addDoc, collection, onSnapshot } from 'firebase/firestore'
 import { useSession } from 'next-auth/react'
 import { useState } from 'react'
 import LoadingSpinner from './LoadingSpinner'
+import { useSubscriptionStore } from '@/store/store'
+import ManageAccountButton from './ManageAccountButton'
 
 const CheckoutButton = () => {
 	const { data: session } = useSession()
 	const [loading, setLoading] = useState(false)
+	const subscription = useSubscriptionStore((state) => state.subscription)
+
+	const isLoadingSubscription = subscription === undefined
+	const isSubscribed =
+		subscription?.status === 'active' &&
+		subscription?.items[0]?.plan.active === true
+
 	const createCheckoutSession = async () => {
 		if (!session?.user.id) return
 		// Push a document into firestore db
@@ -42,12 +51,15 @@ const CheckoutButton = () => {
 	return (
 		<div className='flex flex-col space-y-2'>
 			{/* IF Subscribed:  show me the user is subscribed */}
-			<button
-				onClick={() => createCheckoutSession()}
-				className='mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-60'
-			>
-				{loading ? <LoadingSpinner /> : 'Sign Up'}
-			</button>
+			<div className='mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-60'>
+				{isSubscribed ? (
+					<ManageAccountButton />
+				) : isLoadingSubscription || loading ? (
+					<LoadingSpinner />
+				) : (
+					<button onClick={() => createCheckoutSession()}>'Sign Up'</button>
+				)}
+			</div>
 		</div>
 	)
 }
--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -14,6 +14,7 @@ import { signIn, signOut } from 'next-auth/react'
 import { useSubscriptionStore } from '@/store/store'
 import LoadingSpinner from './LoadingSpinner'
 import { StarIcon } from 'lucide-react'
+import ManageAccountButton from './ManageAccountButton'
 
 const UserButton = ({ session }: { session: Session | null }) => {
 	const subscription = useSubscriptionStore((state) => state.subscription)
@@ -51,8 +52,7 @@ const UserButton = ({ session }: { session: Session | null }) => {
 
 							<DropdownMenuSeparator />
 							<DropdownMenuItem>
-								Manage
-								{/* <ManageAccountButton /> */}
+								<ManageAccountButton />
 							</DropdownMenuItem>
 						</>
 					)}
