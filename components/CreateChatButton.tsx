804bc5d060e7668c76215d300b1c10c307303d6d
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-03-31 22:28:38 +0300

    Added CreateChatButton functionality, able to create new chats and store uuid in the db

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -9,6 +9,8 @@ import { useSubscriptionStore } from '@/store/store'
 import LoadingSpinner from './LoadingSpinner'
 import { useToast } from './ui/use-toast'
 import { v4 as uuidv4 } from 'uuid'
+import { serverTimestamp, setDoc } from 'firebase/firestore'
+import { addChatRef } from '@/lib/converters/ChatMembers'
 
 const CreateChatButton = ({ isLarge }: { isLarge?: boolean }) => {
 	const { data: session } = useSession()
@@ -32,12 +34,50 @@ const CreateChatButton = ({ isLarge }: { isLarge?: boolean }) => {
 		// ----
 
 		const chatId = uuidv4()
-		router.push(`/chat/${chatId}`)
+
+		await setDoc(addChatRef(chatId, session.user.id), {
+			userId: session.user.id!,
+			email: session.user.email!,
+			timestamp: serverTimestamp(),
+			isAdmin: true,
+			chatId: chatId,
+			image: session.user.image || '',
+		})
+			.then(() => {
+				toast({
+					title: 'Success',
+					description: 'Your chat has been created!',
+					className: 'bg-green-600 text-white',
+					duration: 2000,
+				})
+				router.push(`/chat/${chatId}`)
+			})
+			.catch((error) => {
+				console.error(error)
+				toast({
+					title: 'Error',
+					description: 'There was an error creating your chat!',
+					variant: 'destructive',
+				})
+			})
+			.finally(() => {
+				setLoading(false)
+			})
 	}
+
+	if (isLarge) {
+		return (
+			<div>
+				<Button variant={'default'} onClick={createNewChat}>
+					{loading ? <LoadingSpinner /> : 'Create a New Chat'}
+				</Button>
+			</div>
+		)
+	}
+
 	return (
 		<Button onClick={createNewChat} variant={'ghost'}>
-			{loading ? <LoadingSpinner /> : 'Create a New Chat'}
-			{/* <MessageSquarePlusIcon /> */}
+			<MessageSquarePlusIcon />
 		</Button>
 	)
 }
