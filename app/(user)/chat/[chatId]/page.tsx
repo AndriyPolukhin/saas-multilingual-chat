76a3df6861cd2db4826892202a2777bb75109c83
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-04-02 00:55:05 +0300

    Added ChatInput component

--- app/(user)/chat/[chatId]/page.tsx
+++ app/(user)/chat/[chatId]/page.tsx
@@ -1,13 +1,25 @@
+import { authOptions } from '@/auth'
+import ChatInput from '@/components/ChatInput'
+import { getServerSession } from 'next-auth'
 
-const ChatPage = () => {
-  return (
-    <div>
-        {/* Admin Controls */}
-        {/* Chat Members Badge */}
-        {/* Chat Messages */}
-        {/* Chat Input */}
-    </div>
-  )
+type Props = {
+	params: {
+		chatId: string
+	}
+}
+
+const ChatPage = async ({ params: { chatId } }: Props) => {
+	const session = await getServerSession(authOptions)
+
+	return (
+		<div>
+			{/* Admin Controls */}
+			{/* Chat Members Badge */}
+			{/* Chat Messages */}
+			{/* Chat Input */}
+			<ChatInput chatId={chatId} />
+		</div>
+	)
 }
 
 export default ChatPage
